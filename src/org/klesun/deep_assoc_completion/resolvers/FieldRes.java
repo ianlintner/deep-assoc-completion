a4f8ae0705953a1b7f49ff0e1011737c93ddbd67
Artur Klesun <arturklesun@gmail.com>
2018-08-20 01:48:15 +0300

    Unfinished: Do not use deep object resolution in FieldRes when not needed

--- src/org/klesun/deep_assoc_completion/resolvers/FieldRes.java
+++ src/org/klesun/deep_assoc_completion/resolvers/FieldRes.java
@@ -3,6 +3,7 @@ package org.klesun.deep_assoc_completion.resolvers;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.lang.psi.elements.Field;
 import com.jetbrains.php.lang.psi.elements.Function;
 import com.jetbrains.php.lang.psi.elements.Method;
 import com.jetbrains.php.lang.psi.elements.PhpExpression;
@@ -11,6 +12,7 @@ import org.klesun.deep_assoc_completion.*;
 import org.klesun.deep_assoc_completion.helpers.FuncCtx;
 import org.klesun.deep_assoc_completion.helpers.MultiType;
 import org.klesun.deep_assoc_completion.resolvers.var_res.AssRes;
+import org.klesun.deep_assoc_completion.resolvers.var_res.DocParamRes;
 import org.klesun.lang.Lang;
 import org.klesun.lang.Opt;
 import org.klesun.lang.Tls;
@@ -46,22 +48,31 @@ public class FieldRes extends Lang
         return aFunc.equals(bFunc);
     }
 
+    private
+
     public MultiType resolve(FieldReferenceImpl fieldRef)
     {
         L<DeepType> result = list();
-        MultiType objMt = opt(fieldRef.getClassReference())
-            .map(ref -> ctx.clsIdeaType
-                .flt(typ -> ref.getText().equals("static"))
-                .flt(typ -> ArrCtorRes.resolveIdeaTypeCls(typ, ref.getProject()).size() > 0)
-                .map(typ -> new MultiType(list(new DeepType(ref, typ))))
-                .def(ctx.findExprType(ref)))
-            .def(MultiType.INVALID_PSI);
+        S<MultiType> getObjMt = Tls.onDemand(() -> opt(fieldRef.getClassReference())
+            .fop(ref -> Opt.fst(
+                () -> ctx.clsIdeaType
+                    .flt(typ -> ref.getText().equals("static"))
+                    .flt(typ -> ArrCtorRes.resolveIdeaTypeCls(typ, ref.getProject()).size() > 0)
+                    .map(typ -> new MultiType(list(new DeepType(ref, typ)))),
+                () -> opt(ctx.findExprType(ref))
+            ))
+            .def(MultiType.INVALID_PSI));
 
-        objMt.getProps()
-            .flt(prop -> prop.name.equals(fieldRef.getName()))
-            .fch(prop -> result.addAll(prop.getTypes()));
+        L<Field> declarations = Opt.fst(
+            () -> L(fieldRef.multiResolve(false))
+                .map(reses -> reses.map(res -> res.getElement())),
+            () -> opt(getObjMt.get())
+                .fop(mt -> ArrCtorRes.resolveMtCls(mt, fieldRef.getProject()))
+                .fap(cls -> L(cls.getFields()))
+                .flt(f -> f.getName().equals(fieldRef.getName()))
+        ).fap(a -> a);
 
-        ArrCtorRes.resolveMtCls(objMt, fieldRef.getProject())
+        ArrCtorRes.resolveMtCls(getObjMt, fieldRef.getProject())
             .fap(cls -> L(cls.getFields()))
             .flt(f -> f.getName().equals(fieldRef.getName()))
             .fch(resolved -> {
@@ -71,6 +82,19 @@ public class FieldRes extends Lang
                     .fop(toCast(PhpExpression.class))
                     .map(def -> implCtx.findExprType(def).types)
                     .thn(result::addAll);
+
+                L<Assign> docAsses = opt(resolved.getContainingClass())
+                    .map(cls -> cls.getDocComment())
+                    .fap(doc -> L(doc.getPropertyTags()))
+                    .flt(tag -> opt(tag.getProperty()).flt(pro -> pro.getName().equals(fieldRef.getName())).has())
+                    .map(tag -> {
+                        S<MultiType> getMt = () -> new DocParamRes(ctx).resolve(tag).def(MultiType.INVALID_PSI);
+                        return new Assign(list(), getMt, true, tag, tag.getType());
+                    })
+                    ;
+
+                System.out.println("zhopa " + fieldRef.getText());
+                System.out.println("pizda " + Tls.implode("; ", docAsses.map(ass -> ass.assignedType.get() + "")));
 
                 L<Assign> asses = opt(resolved.getContainingFile())
                     .map(file -> findReferences(file, fieldRef.getName()))
@@ -92,9 +116,13 @@ public class FieldRes extends Lang
                         })
                         .fop(methCtx -> (new AssRes(methCtx)).collectAssignment(assPsi, false)));
 
-                List<DeepType> types = AssRes.assignmentsToTypes(asses);
+                List<DeepType> types = AssRes.assignmentsToTypes(docAsses.cct(asses));
                 result.addAll(types);
             });
+
+        getObjMt.getProps()
+            .flt(prop -> prop.name.equals(fieldRef.getName()))
+            .fch(prop -> result.addAll(prop.getTypes()));
 
         return new MultiType(result);
     }
